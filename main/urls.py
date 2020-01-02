85b89bbe48e5dea6bab5a717c30784118d91147b
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-01-02 19:30:21 +0700

    models template

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('<int:id>', views.home, name='home'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList, Item
 
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1>Hi Rief</h1>")
+def home(request, id):
+    todo_list = TodoList.objects.get(id=id)
+    item_list = todo_list.item_set.all()
+    context = {
+        'todo_list': todo_list,
+        'item_list': item_list,
+    }
+    return render(request, 'main/home.html', context)
+
 
